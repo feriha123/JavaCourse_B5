63a2dff499f55c5c5879e4eff710126af1f685e2
hmk10 <halitmk@gmail.com>
2021-10-26 19:57:49 -0400

    Day 14 - For Loop Task

---
+++ src/Day14/Task_2_SumNumbersFromUser.java
@@ -0,0 +1,9 @@
+package Day14;
+
+public class Task_2_SumNumbersFromUser {
+
+    // get limit from user
+    // write a java program which calculates the sum of numbers [0 to limit];
+    // using for-loop
+
+}
