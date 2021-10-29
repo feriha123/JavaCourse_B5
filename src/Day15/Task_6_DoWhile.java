f0a2bffd34993fc5f2e2bd49e39de78551a90fa0
hmk10 <halitmk@gmail.com>
2021-10-28 21:43:01 -0400

    Day 15 - Do - While Loop

--- src/Day15/Task_6_DoWhile.java
+++ src/Day15/Task_6_DoWhile.java
@@ -6,7 +6,12 @@ public class Task_6_DoWhile {
 
     public static void main(String[] args) {
 
+        int counter = 1;
 
+        do {
+            System.out.println(counter);
+            counter += 2;
+        } while (counter <= 30);
 
     }
 
